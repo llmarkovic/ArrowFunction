126d1e6c3c7faa7f2cdac7f496be15575dd0dece
Luka Markovic <markovicluka29@gmail.com>
2020-07-05 23:01:50 +0200

    Exercise

---
+++ ArrowFunction.js
@@ -0,0 +1,47 @@
+// function message(a) {
+//     return `Hello ${a}`;
+// }
+// console.log(message('John'));
+//
+
+
+// const message = (a) => `Hello ${a}`
+
+// console.log(message('John'));
+
+
+// const people = [
+//     {
+//         fName: 'John',
+//         lName: 'Smith'
+//     },
+//     {
+//         fName: 'Mary',
+//         lName: 'Clark'
+//     }
+// ];
+
+// console.log(people.map(person => person.lName));
+
+
+// let demo = function() {
+//     console.log(this);
+// }
+
+// window.addEventListener("load", demo);
+
+// document.getElementById("btn").addEventListener("click", demo)
+
+
+
+
+// function stopwatch() {
+//     this.time = 0;
+
+// setInterval(() => {
+//     this.time++;
+//     console.log(this.time);
+// }, 1000);
+// }
+
+// let timer = new stopwatch();
